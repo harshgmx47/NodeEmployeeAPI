d40812956e305f2078bab850d7eab4807471fe26
harsh vadera <harshgmx47@gmail.com>
2024-06-22 12:06:48 +0530

    changes

--- api/index.js
+++ api/index.js
@@ -1,35 +1,43 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const employeeRoutes = require('../routes/employee');
+const employeeRoutes = require('../routes/employees');
+require('dotenv').config();
 
 const app = express();
 
 app.use(bodyParser.json());
 
-// MongoDB connection
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+// Error handling for JSON parsing errors
+app.use((err, req, res, next) => {
+  if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
+    console.error('Bad JSON');
+    return res.status(400).send({ status: 400, message: 'Bad JSON' }); // Bad request
+  }
+  next();
+});
 
-// Use employee routes
-app.use('/api/employees', employeeRoutes);
+// MongoDB connection
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch(err => {
+  console.error('Failed to connect to MongoDB', err);
+});
 
 // Health check endpoint
 app.get('/api/health', (req, res) => {
   res.status(200).json({ status: 'ok' });
 });
 
+// Use employee routes
+app.use('/api/employees', employeeRoutes);
 
-const port =process.env.PORT  || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
 
 module.exports = app;
-
-
-
-
-
-
-
-
---
+++ routes/employees.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const router = express.Router();
+const Employee = require('../model/employee')
+
+
+// Get all Employee
+router.get('/',async(req,res)=> {
+    try {
+        const employee =await Employee.find({});
+        res.json(employee);
+    } catch (error) {
+        res.status(500).send(error);
+        
+    }
+
+});
+
+// Helper function to parse date string
+const parseDate = (dateString) => {
+    const [day, month, year] = dateString.split('-');
+    return new Date(`${year}-${month}-${day}`);
+  };
+  
+  // Add Employee
+  router.post('/', async (req, res) => {
+    try {
+      const { name, joinDate, active } = req.body;
+      const parsedJoinDate = parseDate(joinDate);
+      const newEmployee = new Employee({ name, joinDate: parsedJoinDate, active });
+  
+      await newEmployee.save();
+      res.status(200).send(newEmployee);
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  });
+
+module.exports = router;
